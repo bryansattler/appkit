1f8fcb8bddc161926a522d70957339ca1bfa199f
Fabian Zeindl <zeindl@partner4media.com>
2012-04-26 18:12:18 +0200

    added helper for GridLayout

---
+++ src/org/appkit/widget/util/GridLayoutUtils.java
@@ -0,0 +1,41 @@
+package org.appkit.widget.util;
+
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Control;
+
+public final class GridLayoutUtils {
+
+	//~ Methods --------------------------------------------------------------------------------------------------------
+
+	/** 
+	 * includes a control in the GridLayout and adjusts the columns-number accordingly
+	 * <b>Caution:</b> this is meant to be used in single-row layouts only
+	 * 
+	 */
+	public static void show(Control control) {
+		GridData data = (GridData) control.getLayoutData();
+		GridLayout layout = (GridLayout) control.getParent().getLayout();
+		
+		if (data.exclude)  {
+			layout.numColumns = layout.numColumns + 1;
+			data.exclude = false;
+		}
+	}
+
+	/** 
+	 * excludes a control from the GridLayout and adjusts the columns-number accordingly
+	 * <b>Caution:</b> this is meant to be used in single-row layouts only
+	 * 
+	 */
+	public static void hide(Control control) {
+		GridData data = (GridData) control.getLayoutData();
+		GridLayout layout = (GridLayout) control.getParent().getLayout();
+		
+		if (!data.exclude)  {
+			layout.numColumns = layout.numColumns - 1;
+			data.exclude = true;
+		}
+	}
+
+}
