e9c68952791bc070a3b6c2f5ab7c178e920d0ee2
Fabian Zeindl <zeindl@partner4media.com>
2012-07-01 20:15:12 +0200

    BUGFIX: timeUnits weren't calculated correctly in SmartExecutor

--- src/org/appkit/util/RepeatingRunnable.java
+++ src/org/appkit/util/RepeatingRunnable.java
@@ -7,19 +7,21 @@ final class RepeatingRunnable extends DelayedRunnable {
 
 	//~ Instance fields ------------------------------------------------------------------------------------------------
 
-	private final long periodInMillis;
+	private final long period;
+	private final TimeUnit delayUnit;
 
 	//~ Constructors ---------------------------------------------------------------------------------------------------
 
 	public RepeatingRunnable(final Runnable runnable, final long period, final TimeUnit delayUnit) {
 		super(runnable, period, delayUnit);
 
-		this.periodInMillis = delayUnit.convert(period, TimeUnit.MILLISECONDS);
+		this.period		   = period;
+		this.delayUnit     = delayUnit;
 	}
 
 	//~ Methods --------------------------------------------------------------------------------------------------------
 
 	public RepeatingRunnable reschedule() {
-		return new RepeatingRunnable(this.runnable, this.periodInMillis, TimeUnit.MILLISECONDS);
+		return new RepeatingRunnable(this.runnable, this.period, this.delayUnit);
 	}
 }
